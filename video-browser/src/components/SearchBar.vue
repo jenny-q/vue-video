00bf3cde7737bb7370d785760af4bf5d43beea4b
jennyq <jenny.q@me.com>
2021-08-17 17:27:28 -0400

    + bootstrap, video components, deleting default files
    axios, notes

---
+++ video-browser/src/components/SearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <section>
+        <!-- <input v-on:input="onInput"/> -->
+        <input @input="onInput"/>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    methods: { // add events
+        onInput: function(event) {
+            this.$emit('termChange', event.target.value);
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
---
+++ video-browser/src/components/VideoList.vue
@@ -0,0 +1,24 @@
+<template>
+    <ul>
+        <VideoListItem 
+            v-for="video in videos"
+            :video="video"
+            :key="video.etag">
+        </VideoListItem>
+    </ul>
+</template>
+
+<script>
+import VideoListItem from './VideoListItem';
+export default {
+    name: 'VideoList',
+    components: {
+        VideoListItem
+    },
+    props: ['videos']
+}
+</script>
+
+<style>
+
+</style>
